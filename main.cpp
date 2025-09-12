6ed2750e635e180402e907c62056da472638bb27
ocanath <ocanath@gmail.com>
2025-09-12 12:16:22 -0700

    comments on strategy, todo

--- main.cpp
+++ main.cpp
@@ -9,6 +9,24 @@
 
 #define BITS_PER_FRAME	40	//2 bytes header, 2 bytes payload, 10 bits per byte = 40 bits
 
+
+/*
+
+TODO: Develop the BLE integrations
+	1. Flag parsing to check for --ble flag, with --name (filter), --connect-by-name(?) and --mac options to skip the command line interactive scanner
+	2. flesh out the scanner - include interactive option (connect or notify examples!), name filtering, or direct mac 
+	3. need to subscribe to the notification service
+	4. need to test writing and receiving
+	5. Need to design the data transfer! Current concept:
+		-MTU double buffer on the embedded side
+		-transfer an MTU. the embedded begins playback via UART. With enough time left in the current buffer playback for a new MTU transfer, the embedded system will trigger a notification. 
+		-Upon reception of that notification, a new MTU will be transferred, and the client will wait for another notification before transferring the next one.
+	Potential issues:
+		-notification latency or dropped frames leading to skips, drops, or complete failure. 
+		-Some back of envelope analysis: an MTU is 247 bytes. That means 246 bytes -> 123 frames. At an encoding of 17631 frames per second, that means an MTU will buffer out in 6.97ms.
+		The minimum connection interval is 7.5ms, which means... YOU'RE FUCKED because you need to send data faster than that. rip
+*/
+
 int main(int argc, char** argv) 
 {
     if (argc < 2) {
