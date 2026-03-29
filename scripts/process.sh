669f74519a036aee1d473b61930b13079fc6179c
ocanath <ocanath@gmail.com>
2026-03-28 23:29:26 -0700

    good settings

--- scripts/process.sh
+++ scripts/process.sh
@@ -16,7 +16,7 @@ echo "- Applying highpass filter at 2000Hz"
 echo "- Applying lowpass filter at 10000Hz"
 echo "- Scaling volume by 10000/32767 (0.305)"
 
-ffmpeg -i "$INPUT_FILE" -ar 20000 -ac 1 -af "highpass=f=2000,volume=12000/32767" -y "$OUTPUT_FILE"
+ffmpeg -i "$INPUT_FILE" -ar 18182 -ac 1 -af "highpass=f=1000,volume=12000/32767" -y "$OUTPUT_FILE"
 
 if [ $? -eq 0 ]; then
     echo "Success! Output saved as: $OUTPUT_FILE"
