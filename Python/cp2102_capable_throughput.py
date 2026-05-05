651559f8dc06eae5cb2811366f9a0911de8dc5f7
ocanath <ocanath@gmail.com>
2026-05-05 11:26:08 -0700

    basically notes - add estimate for stm32 side

--- Python/cp2102_capable_throughput.py
+++ Python/cp2102_capable_throughput.py
@@ -7,6 +7,15 @@ t_us_from_measurement = 122.63600001460873
 ift_us = get_interframe_delay_us(nbytes_from_measurement, t_us_from_measurement, 921600, 1, 0)
 print(f"Interframe delay is {ift_us} microseconds")
 actual_est_throughput = get_throughput_fps(32, 2, 5, 921600, 1, 0, ift_us)
-print(f"Based on {nbytes_from_measurement} bytes at {t_us_from_measurement} us wire time, your throughput will be:\n    {actual_est_throughput} Hz")
+print(f"From PC, based on {nbytes_from_measurement} bytes at {t_us_from_measurement} us wire time, your throughput will be:\n    {actual_est_throughput} Hz")
 
 
+
+
+
+tus_from_STM32 = 204.5580000640257
+nbytes_from_STM32 = 19
+ift_us = get_interframe_delay_us(nbytes_from_STM32, tus_from_STM32, 921600, 1, 0)
+print(f"On stm32, interframe delay is {ift_us}")
+actual_est_throughput = get_throughput_fps(32, 2, 5, 921600, 1, 0, ift_us)
+print(f"From STM32, you can expect {actual_est_throughput} Hz output freq")
