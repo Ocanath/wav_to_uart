52bd4e9098b5608b465ad617ab17ef3bccee4d8d
ocanath <ocanath@gmail.com>
2026-05-05 11:46:48 -0700

    updated meas

--- Python/cp2102_capable_throughput.py
+++ Python/cp2102_capable_throughput.py
@@ -1,7 +1,7 @@
 from wiretime import get_interframe_delay_us, get_throughput_fps
 
 nbytes_from_measurement = 9
-t_us_from_measurement = 122.63600001460873
+t_us_from_measurement = 123.141
 
 
 ift_us = get_interframe_delay_us(nbytes_from_measurement, t_us_from_measurement, 921600, 1, 0)
@@ -13,8 +13,8 @@ print(f"From PC, based on {nbytes_from_measurement} bytes at {t_us_from_measurem
 
 
 
-tus_from_STM32 = 204.5580000640257
-nbytes_from_STM32 = 19
+tus_from_STM32 = 335
+nbytes_from_STM32 = 31
 ift_us = get_interframe_delay_us(nbytes_from_STM32, tus_from_STM32, 921600, 1, 0)
 print(f"On stm32, interframe delay is {ift_us}")
 actual_est_throughput = get_throughput_fps(32, 2, 5, 921600, 1, 0, ift_us)
