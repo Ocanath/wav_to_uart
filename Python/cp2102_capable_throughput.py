d7b1d7835ca0f8c5d6c612cbcdeb9ae58d17f895
ocanath <ocanath@gmail.com>
2026-05-05 12:09:10 -0700

    estimation of what the average throughput should be and it's too high, based on what i actually get from device

--- Python/cp2102_capable_throughput.py
+++ Python/cp2102_capable_throughput.py
@@ -1,21 +1,42 @@
-from wiretime import get_interframe_delay_us, get_throughput_fps
+from wiretime import get_interframe_delay_us, get_throughput_fps, wire_time
+
+serial_overhead = 5
+
 
 nbytes_from_measurement = 9
 t_us_from_measurement = 123.141
 
 
-ift_us = get_interframe_delay_us(nbytes_from_measurement, t_us_from_measurement, 921600, 1, 0)
-print(f"Interframe delay is {ift_us} microseconds")
-actual_est_throughput = get_throughput_fps(32, 2, 5, 921600, 1, 0, ift_us)
+pc_ift_us = get_interframe_delay_us(nbytes_from_measurement, t_us_from_measurement, 921600, 1, 0)
+print(f"Interframe delay is {pc_ift_us} microseconds")
+actual_est_throughput = get_throughput_fps(32, 2, serial_overhead, 921600, 1, 0, pc_ift_us)
 print(f"From PC, based on {nbytes_from_measurement} bytes at {t_us_from_measurement} us wire time, your throughput will be:\n    {actual_est_throughput} Hz")
-
 
 
 
 
 tus_from_STM32 = 335
 nbytes_from_STM32 = 31
-ift_us = get_interframe_delay_us(nbytes_from_STM32, tus_from_STM32, 921600, 1, 0)
-print(f"On stm32, interframe delay is {ift_us}")
-actual_est_throughput = get_throughput_fps(32, 2, 5, 921600, 1, 0, ift_us)
+stm32_ift_us = get_interframe_delay_us(nbytes_from_STM32, tus_from_STM32, 921600, 1, 0)
+print(f"On stm32, interframe delay is {stm32_ift_us}")
+actual_est_throughput = get_throughput_fps(32, 2, serial_overhead, 921600, 1, 0, stm32_ift_us)
 print(f"From STM32, you can expect {actual_est_throughput} Hz output freq")
+
+
+
+
+stm32_response_time = 18.444e-6		#seconds
+computer_response_time = 230.70e-6
+
+nb_serial_readrequest = 2+2+1+2+2
+nb_max_read_reply = 32	#subject to change
+nframes = (nb_max_read_reply - (serial_overhead + 2)) / 4
+read_request_wiretime = wire_time(nb_serial_readrequest, 921600, 1, 0, pc_ift_us)
+print(f"RRWT {read_request_wiretime*1e6}")
+read_request_response_wiretime = wire_time(nb_max_read_reply, 921600, 1, 0, 0)
+print(f"RRRWT {read_request_response_wiretime*1e6}")
+
+exchange_time_total = (read_request_response_wiretime + stm32_response_time + read_request_response_wiretime + computer_response_time)
+nostream_hz = nframes/exchange_time_total
+print(f"Total time {exchange_time_total}")
+print(f"est dartt non-streaming throughput: {nostream_hz} Hz")
